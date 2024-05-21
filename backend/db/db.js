578ed6e8ec6e604847d2d791749d88a112220656
Prachi Jain <96807804+prachi288@users.noreply.github.com>
2024-05-22 01:34:02 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,8 +1,10 @@
 const mongoose=require('mongoose');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const db=async()=>{
     try{
-        mongoose.set('strictQuery',true)
         await mongoose.connect(process.env.MONGO_URL)
         console.log('DB connected')
     }catch(error){
