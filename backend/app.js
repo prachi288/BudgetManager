233a05751d4815317d85ac096cb903d588aa4e53
Prachi Jain <96807804+prachi288@users.noreply.github.com>
2024-05-22 00:38:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin:"https://budgetclub.netlify.app", credentials:true}));
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
